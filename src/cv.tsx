ff6bae3ffdc6ee6dd2e9159ffc75829b59379342
Cody Zuschlag <cody.zuschlag@gmail.com>
2025-01-19 22:05:24 +0100

    add links

--- src/cv.tsx
+++ src/cv.tsx
@@ -1,11 +1,8 @@
 // eslint-disable-line unicorn/filename-case
 import path, {basename} from 'node:path';
-import {Page, View, Document, render} from '@react-pdf/renderer';
+import {Page, View, Document, render, Link} from '@react-pdf/renderer';
 import {style} from './utils/style.js';
-import SectionHeader from './components/SectionHeader.js';
-import WorkExperience from './components/list/WorkExperience.js';
 import {packageDirectory} from './utils/package-dir.js';
-import {workExperience} from './work-experience.js';
 import Education from './components/sections/Education.js';
 import TechnicalProjects from './components/sections/TechnicalProjects.js';
 import Summary from './components/sections/Summary.js';
@@ -13,8 +10,10 @@ import Conferences from './components/sections/Conferences.js';
 import AwardsMemberships from './components/sections/AwardsMemberships.js';
 import Title from './components/sections/Title.js';
 import Sidebar from './components/sections/Sidebar.js';
-import LinkedInQrCode from './components/LinkedIn.js';
 import Experience from './components/sections/Experience.js';
+import Github from './components/icons/Github.js';
+import LinkedIn from './components/icons/LinkedIn.js';
+import Text from './components/Text.js';
 
 console.log(style('flex-row-reverse items-start text-base'));
 
@@ -38,9 +37,30 @@ function MyDocument() {
             <Title />
             <View
               style={style(
-                'pl-4 pr-12 py-4 text-lg gap-2 leading-tight w-full',
+                'pl-4 pr-12 pt-1 text-lg gap-2 leading-tight w-full',
               )}
             >
+              <View style={style('flex-row w-full pr-8')}>
+                <Link
+                  style={style(
+                    'flex-row items-center gap-1 text-black font-bold',
+                  )}
+                  src="https://linkedin.com/in/cody-zuschlag"
+                >
+                  <LinkedIn style={style('w-5 h-5')} />
+                  <Text>linkedin.com/in/cody-zuschlag</Text>
+                </Link>
+                <View style={style('grow')} />
+                <Link
+                  style={style(
+                    'flex-row items-center gap-1 text-black font-bold',
+                  )}
+                  src="https://github.com/codyzu"
+                >
+                  <Github style={style('w-5 h-5')} />
+                  <Text>github.com/codyzu</Text>
+                </Link>
+              </View>
               <Summary />
               <Education />
               <TechnicalProjects />

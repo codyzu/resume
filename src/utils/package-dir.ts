c7d21763935373a6fd415423cde7d3adf57ab87a
Cody Zuschlag <cody.zuschlag@gmail.com>
2025-01-21 21:46:07 +0100

    debug site

--- src/utils/package-dir.ts
+++ src/utils/package-dir.ts
@@ -1,7 +1,7 @@
 import {packageDirectorySync} from 'pkg-dir';
 
 function getPackageDirectory(): string {
-  const directory = packageDirectorySync();
+  const directory = packageDirectorySync({cwd: import.meta.dirname});
   if (!directory) {
     throw new Error('Could not find package directory');
   }

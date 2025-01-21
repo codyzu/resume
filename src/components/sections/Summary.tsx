133838065e8a1c5c6032575d3fdc4be93010f3db
Cody Zuschlag <cody.zuschlag@gmail.com>
2025-01-21 10:16:23 +0100

    fix missing spaces in summary

--- src/components/sections/Summary.tsx
+++ src/components/sections/Summary.tsx
@@ -3,5 +3,5 @@ import {type Version} from '../../version.js';
 import Text from '../Text.js';
 
 export default function Summary({version}: {readonly version: Version}) {
-  return <Text>{summary[version].replaceAll('\n', '')}</Text>;
+  return <Text>{summary[version].split('\n').filter(Boolean).join(' ')}</Text>;
 }
